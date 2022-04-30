d0c28941505acb87e5139a8d71394fdb3b3fddef
Adrian Serbanscu <adrian@adrianserbanescu.com>
2022-04-30 19:50:13 +0200

    lab1 environ

--- lab1/GetEnvironmentVariables.java
+++ lab1/GetEnvironmentVariables.java
@@ -1,9 +1,7 @@
-import java.util.Properties;
-
 public class GetEnvironmentVariables {
 
     //  Fill in the path to your sp21-s*** folder between the quotes
-    public static String REPO_DIR = "";
+    public static String REPO_DIR = "/home/adrianaris/Treaba/datastructures";
 
     //  Fill in the path to your snaps-sp21-s*** folder between the quotes
     public static String SNAPS_DIR = "";
