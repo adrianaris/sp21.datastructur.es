9d3b39176930bb70e8a5c8400bcf0519b8ba98b4
Adrian Serbanscu <adrian@adrianserbanescu.com>
2022-05-01 09:17:08 +0200

    Collatz

--- lab1/GetEnvironmentVariables.java
+++ lab1/GetEnvironmentVariables.java
@@ -1,13 +1,13 @@
 public class GetEnvironmentVariables {
 
     //  Fill in the path to your sp21-s*** folder between the quotes
-    public static String REPO_DIR = "/home/adrianaris/Treaba/datastructures";
+    public static String REPO_DIR = "/home/adrianaris/Treaba/datastructures/";
 
     //  Fill in the path to your snaps-sp21-s*** folder between the quotes
     public static String SNAPS_DIR = "";
 
     // Fill in the type of your shell by running 'echo $0` in your terminal. It should be zsh or bash.
-    public static String SHELL = "";
+    public static String SHELL = "zsh";
 
     public static void main(String[] args) {
 
