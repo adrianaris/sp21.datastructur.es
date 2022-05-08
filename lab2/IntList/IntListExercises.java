4ccf9766d25c00a68594cf339bcdcc42da309184
Adrian Serbanscu <adrian@adrianserbanescu.com>
2022-05-08 19:13:51 +0200

    lab2 part B

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -52,7 +52,7 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
